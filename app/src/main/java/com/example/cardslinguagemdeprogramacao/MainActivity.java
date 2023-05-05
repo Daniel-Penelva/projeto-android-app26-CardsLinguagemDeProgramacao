9bf0d933c629d6fd8f020efdb5355c2ffbf76e8d
Daniel Penelva <d4n.andrade@gmail.com>
2023-05-05 19:09:56 -0300

    Implementando a Classe ArrayAdapter

--- app/src/main/java/com/example/cardslinguagemdeprogramacao/MainActivity.java
+++ app/src/main/java/com/example/cardslinguagemdeprogramacao/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.cardslinguagemdeprogramacao;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -82,6 +89,73 @@ class DadosLinguagemProgramacao{
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+}
+
+class MeuAdaptador extends ArrayAdapter{
+
+    // Através do generate vamos criar o construtor e três métodos obrigatórios, são eles (1)add (2) getCount (3) getView
+
+    // Tras as funcionalidades do ArrayAdapter
+    public MeuAdaptador(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    //
+
+
+    @Override
+    public void add(@Nullable Object object) {
+        super.add(object);
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    /*Este método retorna activity criada, no caso, os objetos são visualizados no layout da minha_celula.xml. Serão pegos os objetos da classe
+    DadosLinguagemProgramacao e do modelo da célula criada na classe ViewLinguagemProgramacao e devolver na função getView da classe MeuAdapter e
+    quando o adaptador for executado vai percorrer o array de dados convertendo todos em objeto do tipo View. */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View minhaView;
+        minhaView = convertView;
+
+        ViewLinguagemProgramacao viewLinguagemProgramacao;
+
+        // Se não existir, então vai ser criada o layout
+        if(convertView == null){
+            // Trás o layout minha_celula.xml
+            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            minhaView = inflater.inflate(R.layout.minha_celula, parent, false);
+
+            // Criar os objetos que serão devolvidos ao layout
+            viewLinguagemProgramacao = new ViewLinguagemProgramacao();
+            viewLinguagemProgramacao.icone = (ImageView) minhaView.findViewById(R.id.imageJava);
+            viewLinguagemProgramacao.titulo = (TextView) minhaView.findViewById(R.id.textTitulo);
+            viewLinguagemProgramacao.descricao = (TextView) minhaView.findViewById(R.id.textDescricao);
+
+            //registrar na minha_celula com os itens da lista
+            minhaView.setTag(viewLinguagemProgramacao);
+
+        }else{
+            // Se existir o layout vai pegar da célula que já existe
+            viewLinguagemProgramacao = (ViewLinguagemProgramacao) minhaView.getTag();
+        }
+
+        DadosLinguagemProgramacao dadosLinguagemProgramacao;
+
+        // Para cada objeto criado é obtido a posição atual da célula
+        dadosLinguagemProgramacao = (DadosLinguagemProgramacao)this.getItem(position);
+
+        viewLinguagemProgramacao.icone.setImageResource(dadosLinguagemProgramacao.getIcone());
+        viewLinguagemProgramacao.titulo.setText(dadosLinguagemProgramacao.getTitulo());
+        viewLinguagemProgramacao.descricao.setText(dadosLinguagemProgramacao.getDescricao());
+
+        return minhaView;
     }
 }
 
